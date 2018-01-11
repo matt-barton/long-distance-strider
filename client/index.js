456098d64e31e5ef01c638cfd3f12ef2686d6da2
Matt Barton <matt@mattbarton.org>
2018-01-11 21:47:45 +0000

    Swapped jade for pug, implemented windows npm tasks

--- client/index.js
+++ client/index.js
@@ -1,6 +1,6 @@
 const express = require('express'),
   app = express(),
-  jade = require('jade'),
+  pug = require('pug'),
   _ = require('underscore'),
   Race = require('../lib/db/race'),
   isAuth = require('../lib/auth_middleware');
@@ -9,7 +9,7 @@ app.get('/', isAuth, (req, res, next) => {
   try {
     Race.find({ ignore: false }, {url: 1, processed: 1}).exec().then(races => {
       console.log(races);
-      var html = jade.compileFile('./client/src/templates/homepage.jade')({
+      var html = pug.compileFile('./client/src/templates/homepage.pug')({
         title: 'Home',
         auth: req.isAuthenticated(),
         newRaces: _(races).where({ processed: false }),
@@ -25,7 +25,7 @@ app.get('/', isAuth, (req, res, next) => {
 /*
 app.get('/login', (req, res, next) => {
   try {
-    var html = jade.compileFile('./client/src/templates/login.jade')({ title: 'Login', auth: req.isAuthenticated() });
+    var html = pug.compileFile('./client/src/templates/login.pug')({ title: 'Login', auth: req.isAuthenticated() });
     res.send(html);
   } catch (e) {
     next(e);
@@ -34,7 +34,7 @@ app.get('/login', (req, res, next) => {
 
 app.get('/about', (req, res, next) => {
   try {
-    var html = jade.compileFile('./client/src/templates/about.jade')({ title: 'About', auth: req.isAuthenticated() });
+    var html = pug.compileFile('./client/src/templates/about.pug')({ title: 'About', auth: req.isAuthenticated() });
     res.send(html);
   } catch (e) {
     next(e);
@@ -43,7 +43,7 @@ app.get('/about', (req, res, next) => {
 
 app.get('/contact', (req, res, next) => {
   try {
-    var html = jade.compileFile('./client/src/templates/contact.jade')({ title: 'Contact', auth: req.isAuthenticated() });
+    var html = pug.compileFile('./client/src/templates/contact.pug')({ title: 'Contact', auth: req.isAuthenticated() });
     res.send(html);
   } catch (e) {
     next(e);
